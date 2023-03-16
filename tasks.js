61bea4e26e461eb4f1049b214e2a9ff7e6a7e356
riFinkelstein <rivkafinkelstein823@gmail.com>
2023-03-16 17:09:13 +0200

    addded piroity imput wiht ability to search by priority nummber

--- tasks.js
+++ tasks.js
@@ -1,21 +1,34 @@
 // JavaScript code
 var taskList = document.getElementById("taskList");
 var taskInput = document.getElementById("taskInput");
+var priorityInput = document.getElementById("priorityInput");
 var form = document.getElementById("taskForm");
 var error = document.getElementById("error");
-
+var searchInput = document.getElementById("searchInput");
 
 var tasks = [];
-
 
 function addTask() {
     if (taskInput.value === "") {
         alert("Please enter a task.");
     }
+    // Create a new object for the task
+    var task = {
+        name: taskInput.value,
+        priority: priorityInput.value
+    };
+
+    // Add the new task to the tasks array
+    tasks.push(task);
+
+    // Get the priority value
+    var priority = priorityInput.value;
 
     // Create a new list item
     var li = document.createElement("li");
-    var taskText = document.createTextNode(taskInput.value);
+
+    // Create task text with priority
+    var taskText = document.createTextNode(taskInput.value + " (" + priority + ")");
     li.appendChild(taskText);
 
     // Create a checkbox for the completed task
@@ -38,24 +51,10 @@ function addTask() {
     deleteButton.onclick = deleteTask;
     li.appendChild(deleteButton);
 
-
     taskList.appendChild(li);
-
-    // Add the new task to the tasks array
-    tasks.push(li);
-
-    // Remove the deleted task from the tasks array
-    var index = tasks.indexOf(li);
-    if (index > -1) {
-        tasks.splice(index, 1);
-    }
-
 
     taskInput.value = "";
     error.textContent = "";
-
-
-    ;
 }
 
 form.addEventListener("submit", function (event) {
@@ -134,13 +133,17 @@ function saveTask() {
 function searchTasks() {
     // Get the search query
     var query = searchInput.value.toLowerCase();
+    var priority = parseInt(query); // convert search query to number
+
 
     // Loop through all the list items
     for (var i = 0; i < taskList.children.length; i++) {
         var li = taskList.children[i];
+        var taskPriority = parseInt(tasks[i].priority);
+
 
         // Check if the task matches the search query
-        if (li.textContent.toLowerCase().includes(query)) {
+        if (li.textContent.toLowerCase().includes(query) || taskPriority === priority) {
             li.style.display = "";
         } else {
             li.style.display = "none";
@@ -156,30 +159,7 @@ function searchTasks() {
     }
 }
 
-function sortTasks() {
-    var sortOption = sortSelect.value;
 
-    switch (sortOption) {
-        case "priority-asc":
-            tasks.sort(function (a, b) {
-                return a.childNodes[1].value - b.childNodes[1].value;
-            });
-            break;
-        case "priority-desc":
-            tasks.sort(function (a, b) {
-                return b.childNodes[1].value - a.childNodes[1].value;
-            });
-            break;
-    }
-
-    while (taskList.firstChild) {
-        taskList.removeChild(taskList.firstChild);
-    }
-
-    for (var i = 0; i < tasks.length; i++) {
-        taskList.appendChild(tasks[i]);
-    }
-}
 
 searchInput.addEventListener("input", searchTasks);
 var addButton = document.getElementById("addButton");
